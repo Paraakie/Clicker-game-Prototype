599c7b1416724458216d350cdb60a5dedf0cacc1
Paraakie <sven.m.gerhards@gmail.com>
2019-10-09 12:32:21 +1300

    Basic UI
    Wood - Main Resource
    Can be increased by only clicking the "Collect Branch "button
    
    Basic UI look done, but no further functionality

---
+++ Wood/Assets/Scripts/UpdateValues.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateValues : MonoBehaviour
+{
+    //Singular GameObject
+    public GameObject wood;
+    //create empty object based on Update_WoodText script
+    Update_WoodText woodScript;
+
+    private void Start()
+    {
+        //Use Gameobject to access Script values
+        woodScript = wood.GetComponent<Update_WoodText>();
+    }
+
+    //used tp update wood value
+    public void UpdateWood(int woodChange)
+    {
+
+        //Get current Wood Value
+        int newWood = woodScript.GetWoodNum();
+
+        //Calculate new Wood Value
+        newWood = newWood + woodChange;
+
+        //Set new Wood Value
+        woodScript.SetWoodNum(newWood);
+
+        Debug.Log("This is the new Wood Value " + newWood);
+
+        if (newWood < 0)
+        {
+            Debug.Log("Error: Invalid wood value.");
+        }
+    }
+}
---
+++ Wood/Assets/Scripts/Update_WoodText.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Update_WoodText : MonoBehaviour
+{
+    private int woodNum = 0;
+    public Text woodText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Text Displayed
+        woodText.text = "Wood: "+ GetWoodNum();
+
+        //Test function grants wood when pressing space
+        /*
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SetWoodNum(GetWoodNum() + 1);
+        }
+        */
+    }
+
+    /*
+     * Getters and Setters 
+     */ 
+
+    public int GetWoodNum()
+    {
+        return woodNum;
+    }
+
+    public void SetWoodNum(int newWoodNum)
+    {
+        woodNum = newWoodNum;
+    }
+}
+
