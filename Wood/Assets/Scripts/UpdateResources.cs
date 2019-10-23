db55f6bbcd9cb2467f9cb4176e797c3cb7859a17
Paraakie <sven.m.gerhards@gmail.com>
2019-10-23 15:14:49 +1300

    Removed Minion as a resource
    and overwholed base UI
    Also added Tree Image

--- Wood/Assets/Scripts/UpdateResources.cs
+++ Wood/Assets/Scripts/UpdateResources.cs
@@ -9,16 +9,11 @@ public class UpdateResources : MonoBehaviour
     //create Update_WoodText script object
     Update_WoodText woodScript;
 
-    //GameObject for Minions
-    public GameObject minion;
-    //create Update_MinionText script object
-    Update_MinionText minionScript;
-
+    
     private void Start()
     {
         //Use Gameobject to initialise empty script objects
         woodScript = wood.GetComponent<Update_WoodText>();
-        minionScript = minion.GetComponent<Update_MinionText>();
     }
 
     /*
@@ -45,33 +40,5 @@ public class UpdateResources : MonoBehaviour
     }
 
 
-    /*
-     * Update minion Value based on (+/-)int
-     */
-    public void UpdateMinions(int minionsChange)
-    {
-        //Get current Minion Value
-        int newMinions = minionScript.GetMinionNum();
-
-        //Calculate new Mininon Value
-        newMinions = newMinions + minionsChange;
-
-        //Prevent Negative Minion Value
-        if (newMinions < 0)
-        {
-            Debug.Log("Error: Invalid minion value. Minion Value was not updated.");
-            return;
-        }
-
-        //Set new Minion Value
-        minionScript.SetMinionNum(newMinions);
-    }
-
-    /*
-     * General Function for Updating Resources based on (+/-)int
-     */ 
-    public void UpdateResource(int resourceChange/**/)
-    {
-        return;
-    }
+    
 }
