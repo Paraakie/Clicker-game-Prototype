ca5eee7e97d0cfc77e634412ea0f6c15d43a48b2
Paraakie <sven.m.gerhards@gmail.com>
2019-11-03 15:28:06 +1300

    Added RangerNUmText Script

---
+++ Wood/Assets/Scripts/TextScripts/RangerNumText.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RangerNumText : MonoBehaviour
+{
+    public Text rangerNumText;
+    //Number of Rangers
+    private int rangerNum = 0;
+    // Wood Produced per Ranger/sec
+    private double rangerProductivity = 1.0;
+    //Rangers total Wood produced per second
+    private double rangerWpS = 0.0;
+
+    
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        rangerNumText.text = rangerNum.ToString();
+        rangerWpS = rangerNum * rangerProductivity;
+    }
+
+    /**
+     * Getters and Setters
+     */
+
+    // rangerNum
+    public int GetRangerNum()
+    {
+        return rangerNum;
+    }
+
+    public void SetRangerNum(int value)
+    {
+        rangerNum = value;
+    }
+
+    // rangerProductivity
+    public double GetRangerProductivity()
+    {
+        return rangerProductivity;
+    }
+
+    public void SetRangerProductivity(double value)
+    {
+        rangerProductivity = value;
+    }
+
+    // rangerWpS
+    public double GetRangerWpS()
+    {
+        return rangerWpS;
+    }
+
+    public void SetRangerWpS(double value)
+    {
+        rangerWpS = value;
+    }
+
+}
